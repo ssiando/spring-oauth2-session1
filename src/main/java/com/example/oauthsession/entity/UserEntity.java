f4ce6057959e30649d52da5b9ed4b52241f978bd
ssiando <jeremie_han@naver.com>
2024-05-28 16:04:10 +0900

    first commit

---
+++ src/main/java/com/example/oauthsession/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package com.example.oauthsession.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "oauth2_user")
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String role;
+}
